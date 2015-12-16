67f5f85bcdf5f84c86c750f586abf4f66caf6936
Dominic May <jack.thatch@gmail.com>
2015-12-16 21:43:11 +0800

    Added a somewhat complete setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+    name='houndipy',
+    version='0.0.1',
+    author='Mause',
+    author_email='me@mause.me',
+    url='https://github.com/Mause/houndipy',
+    license='MIT',
+    install_requires=['arrow', 'requests'],
+    packages=['houndipy'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
